7145ca50b482d5cdac318ef106fa8b93d02fbf7b
AHMAD ALSHAMI <amfna511@uowmail.edu.au>
2024-11-25 09:50:28 -0500

    AHMAD ALSHAMI Module 3

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 app_name = 'djangoapp'
 urlpatterns = [
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path(route='get_cars', view=views.get_cars, name ='getcars')
     # path for login
     # path(route='login', view=views.login_user, name='login'),
 
