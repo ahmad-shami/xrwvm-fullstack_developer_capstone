6a045fe4a92ef9094260455653d13bcb2c00a03a
Ahmad Alshami <ahmad_shami@hotmail.com>
2024-12-04 12:39:34 -0500

    fix urls and import

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -18,12 +18,18 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import path, include
+from django.contrib import admin
+from djangoapp import views   
 
 
 
 
 
 urlpatterns = [
+    
+    path('login/', view=TemplateView.as_view(template_name="index.html")),
+    path(route='login', view=views.login_user, name='login'),
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
     path('dealers/', TemplateView.as_view(template_name="index.html")),
