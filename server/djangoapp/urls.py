0fe6ce11557db4884761d522cd2c3044f5f78a50
Ahmad Alshami <ahmad_shami@hotmail.com>
2024-12-04 11:01:35 -0500

    correct urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,24 +2,18 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from django.conf import views
+from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
     
     # path for login
     path(route='login', view=views.login_user, name='login'),
-
-    # path for dealer reviews view
-    #path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
-
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     path(route='get_cars', view=views.get_cars, name ='getcars'),
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     path(route='add_review', view=views.add_review, name='add_review'),
-    path(route='login', view=views.login_user, name='login'),
-    path('login/', TemplateView.as_view(template_name="index.html")),
+    #path('login/', TemplateView.as_view(template_name="index.html")),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
