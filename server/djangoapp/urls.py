2bb3645a7816eac5e362f024c72b3a133125bdd0
Ahmad Alshami <ahmad_shami@hotmail.com>
2024-12-06 06:30:34 -0500

    fix login

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,8 +11,8 @@ app_name = 'djangoapp'
 urlpatterns = [
     
     # path for login
-    path('login', view=TemplateView.as_view(template_name="index.html")),
-    path(route='login/', view=views.login_user, name='login'),
+    path('login/', view=TemplateView.as_view(template_name="index.html")),
+    path(route='login', view=views.login_user, name='login'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     path(route='get_cars', view=views.get_cars, name ='getcars'),
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
