cc14d18b74ab60b07db8f3f5df4f3d843ae45c47
Oussama Rahmani <vazer.dpr@gmail.com>
2024-12-13 08:14:17 +0400

    Patch: Added checks to the reviews django api to check for errors from the reviews endpoint before processing.

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -164,17 +164,19 @@ def add_review(request):
         return JsonResponse({"status":403,"message":"Unauthorized"})
 
 def get_dealer_reviews(request, dealer_id):
-    # if dealer id has been provided
-    if(dealer_id):
-        endpoint = "/fetchReviews/dealer/"+str(dealer_id)
-        reviews = get_request(endpoint)
-        for review_detail in reviews:
-            response = analyze_review_sentiments(review_detail['review'])
-            print(response)
-            review_detail['sentiment'] = response['sentiment']
-        return JsonResponse({"status":200,"reviews":reviews})
+    if not dealer_id:
+        return JsonResponse({"status": 400, "message": "Bad Request"})
+
+    endpoint = f"/fetchReviews/dealer/{dealer_id}"
+    reviews = get_request(endpoint)
+
+    if isinstance(reviews, list):
+        for review in reviews:
+            review['sentiment'] = analyze_review_sentiments(review['review'])['sentiment']
     else:
-        return JsonResponse({"status":400,"message":"Bad Request"})
+        reviews = []
+
+    return JsonResponse({"status": 200, "reviews": reviews})
 
 def register_view(request):
     index_path = os.path.join(settings.BASE_DIR, 'static', 'index.html') 
