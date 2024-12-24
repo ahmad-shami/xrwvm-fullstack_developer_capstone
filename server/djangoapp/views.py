0b67bca25358d18361a672db8b07e60dfbdad216
ahmad-shami <ahmad_Shami@hotmail.com>
2024-12-24 12:26:19 +0000

    fix views

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,12 +1,12 @@
 # Uncomment the required imports before adding the code
 
-#from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-#from django.shortcuts import render, redirect
-#from django.contrib.auth import logout
-#from django.contrib import messages
-#from datetime import datetime
+# from django.shortcuts import render, redirect
+# from django.contrib.auth import logout
+# from django.contrib import messages
+# from datetime import datetime
 import os
 from django.conf import settings
 from .models import CarMake, CarModel
@@ -16,14 +16,10 @@ from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
-from .populate import initiate
 from .restapis import get_request, analyze_review_sentiments
-
-
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
-
 
 # Create your views here.
 
@@ -34,7 +30,7 @@ def login_user(request):
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
-    # Try to check if provide credential can be authenticated
+    # Try to check if provided credentials can be authenticated
     user = authenticate(username=username, password=password)
     data = {"userName": username}
     if user is not None:
@@ -43,18 +39,14 @@ def login_user(request):
         data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
 
-
-
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
-    data = {"userName":""}
+    data = {"userName": ""}
     return JsonResponse(data)
 
 # Create a `registration` view to handle sign up request
 @csrf_exempt
 def registration(request):
-    #context = {}
-
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
@@ -62,45 +54,25 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    #email_exist = False
     try:
         # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
     except Exception as e:
-        # If not, simply log this is a new user
-        logger.debug(f"{e} {username} is new user")
+        # If not, simply log that this is a new user
+        logger.debug(f"{e} {username} is a new user")
 
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,password=password, email=email)
+        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password, email=email)
         # Login the user and redirect to list page
         login(request, user)
-        data = {"userName":username,"status":"Authenticated"}
+        data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
-    else :
-        data = {"userName":username,"error":"Already Registered"}
+    else:
+        data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
-
-# # Update the `get_dealerships` view to render the index page with
-# a list of dealerships
-# def get_dealerships(request):
-# ...
-
-# Create a `get_dealer_reviews` view to render the reviews of a dealer
-# def get_dealer_reviews(request,dealer_id):
-# ...
-
-# Create a `get_dealer_details` view to render the dealer details
-# def get_dealer_details(request, dealer_id):
-# ...
-
-# Create a `add_review` view to submit a review
-# def add_review(request):
-# ...
-
-
 
 def get_cars(request):
     # Check if CarMake data exists
@@ -128,39 +100,32 @@ def get_cars(request):
     # Return the car details as a JSON response
     return JsonResponse({"CarModels": cars})
 
-
-
-            #Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
 def get_dealerships(request, state="All"):
-    if(state == "All"):
+    if state == "All":
         endpoint = "/fetchDealers"
     else:
-        endpoint = "/fetchDealers/"+state
+        endpoint = f"/fetchDealers/{state}"
     dealerships = get_request(endpoint)
-    return JsonResponse({"status":200,"dealers":dealerships})
-
+    return JsonResponse({"status": 200, "dealers": dealerships})
 
 def get_dealer_details(request, dealer_id):
-    if(dealer_id):
-        endpoint = "/fetchDealer/"+str(dealer_id)
+    if dealer_id:
+        endpoint = f"/fetchDealer/{dealer_id}"
         dealership = get_request(endpoint)
-        return JsonResponse({"status":200,"dealer":dealership})
+        return JsonResponse({"status": 200, "dealer": dealership})
     else:
-        return JsonResponse({"status":400,"message":"Bad Request"})
-
+        return JsonResponse({"status": 400, "message": "Bad Request"})
 
 def add_review(request):
     if not request.user.is_anonymous:
-        #data = json.loads(request.body)
         try:
-            #response = post_review(data)
-            return JsonResponse({"status":200})
+            # response = post_review(data)
+            return JsonResponse({"status": 200})
         except Exception as e:
             logger.error(f"Error in posting review: {e}")
             return JsonResponse({"status": 401, "message": "Error in posting review"})
-    
     else:
-        return JsonResponse({"status":403,"message":"Unauthorized"})
+        return JsonResponse({"status": 403, "message": "Unauthorized"})
 
 def get_dealer_reviews(request, dealer_id):
     if not dealer_id:
@@ -178,7 +143,7 @@ def get_dealer_reviews(request, dealer_id):
     return JsonResponse({"status": 200, "reviews": reviews})
 
 def register_view(request):
-    index_path = os.path.join(settings.BASE_DIR, 'static', 'index.html') 
+    index_path = os.path.join(settings.BASE_DIR, 'static', 'index.html')
 
     try:
         with open(index_path, 'r') as f:
